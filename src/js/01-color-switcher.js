2969e92d971a8fb2b61e2440d6b016c197a5652c
robertSovar <133514926+robertSovar@users.noreply.github.com>
2023-11-10 19:35:09 +0200

    finished first exercise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timeInterval = null;
+
+
+
+startBtn.addEventListener("click",changeColorInTime);
+stopBtn.addEventListener('click', stopColorChange);
+
+
+function changeColorInTime () {
+   timeInterval = setInterval(getRandomHexColor,1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function stopColorChange () {
+   clearInterval(timeInterval);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+
+
+function getRandomHexColor() {
+    return bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
